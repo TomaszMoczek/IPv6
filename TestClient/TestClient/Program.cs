f9fcab3edf2fd585a24cb8cb160f6dbddcf098f2
TomaszMoczek <t.moczek@gmail.com>
2022-11-19 14:53:19 +0100

    Add files via upload

---
+++ TestClient/TestClient/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 3 && args[0].ToLower().Equals("tcp"))
+                {
+                    IPEndPoint server = new IPEndPoint(IPAddress.Parse(args[1]), Int32.Parse(args[2]));
+                    Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+
+                    socket.Connect(server);
+
+                    if (socket.Connected)
+                    {
+                        Console.WriteLine("Connected with {0} at port {1}", server.Address, server.Port);
+                        Console.WriteLine();
+
+                        {
+                            byte[] data = new byte[1024];
+                            int bytes = socket.Receive(data, data.Length, SocketFlags.None);
+
+                            Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
+                            Console.WriteLine();
+                        }
+
+                        while (true)
+                        {
+                            string text = Console.ReadLine();
+
+                            if (String.IsNullOrEmpty(text))
+                            {
+                                break;
+                            }
+
+                            byte[] data = Encoding.ASCII.GetBytes(text);
+
+                            socket.Send(data, data.Length, SocketFlags.None);
+                        }
+
+                        socket.Close();
+
+                        Console.WriteLine("Disconnected with {0}", server.Address);
+                    }
+                }
+                else if (args.Length == 3 && args[0].ToLower().Equals("udp"))
+                {
+                    IPEndPoint server = new IPEndPoint(IPAddress.Parse(args[1]), Int32.Parse(args[2]));
+                    Socket socket = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
+
+                    socket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.AddMembership, new IPv6MulticastOption(server.Address));
+
+                    Console.WriteLine("Sending UDPv6 Messages to {0} at port {1}", server.Address, server.Port);
+                    Console.WriteLine();
+
+                    while (true)
+                    {
+                        string text = Console.ReadLine();
+                        byte[] data = Encoding.ASCII.GetBytes(text);
+
+                        socket.SendTo(data, server);
+
+                        if (String.IsNullOrEmpty(text))
+                        {
+                            break;
+                        }
+                    }
+
+                    socket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.DropMembership, new IPv6MulticastOption(server.Address));
+                    socket.Close();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Stopped sending the UDPv6 Messages");
+                }
+                else
+                {
+                    throw new Exception("Input arguments are not recognizable !");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestServer/TestServer/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TestServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 2 && args[0].ToLower().Equals("tcp"))
+                {
+                    IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.IPv6Any, Int32.Parse(args[1]));
+                    Socket server = new Socket(AddressFamily.InterNetworkV6, SocketType.Stream, ProtocolType.Tcp);
+
+                    server.Bind(serverEndPoint);
+                    server.Listen(10);
+
+                    Console.WriteLine("Waiting for the TCPv6 Client ...");
+                    Console.WriteLine();
+
+                    Socket client = server.Accept();
+                    IPEndPoint clientEndPoint = (IPEndPoint)client.RemoteEndPoint;
+
+                    Console.WriteLine("Connected with {0} at port {1}:", clientEndPoint.Address, clientEndPoint.Port);
+                    Console.WriteLine();
+
+                    {
+                        byte[] data = Encoding.ASCII.GetBytes("Welcome to the TCPv6 Test Server !");
+                        client.Send(data, data.Length, SocketFlags.None);
+                    }
+
+                    while (true)
+                    {
+                        byte[] data = new byte[1024];
+                        int bytes = client.Receive(data);
+
+                        if (bytes == 0)
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
+                    }
+
+                    client.Close();
+                    server.Close();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Disconnected with {0}", clientEndPoint.Address);
+                }
+                else if (args.Length == 3 && args[0].ToLower().Equals("udp"))
+                {
+                    UdpClient udpClient = new UdpClient(Int32.Parse(args[2]), AddressFamily.InterNetworkV6);
+                    
+                    udpClient.JoinMulticastGroup(IPAddress.Parse(args[1]));
+
+                    Console.WriteLine("Waiting for the UDPv6 Messages ... ");
+                    Console.WriteLine();
+
+                    while (true)
+                    {
+                        IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.IPv6Any, 0);
+                        byte[] data = udpClient.Receive(ref remoteEndPoint);
+                        string message = Encoding.ASCII.GetString(data);
+
+                        if (String.IsNullOrEmpty(message))
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine("Message from {0} at port {1}: {2}", remoteEndPoint.Address, remoteEndPoint.Port, message);
+                    }
+
+                    udpClient.Close();
+
+                    Console.WriteLine();
+                    Console.WriteLine("Stopped waiting for the UDPv6 Messages");
+                }
+                else
+                {
+                    throw new Exception("Input arguments are not recognizable !");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
